ff9afc273ed8283c22e18643daf0be9630e6474a
Alper Mumcular <alper.mumcular@ug.bilkent.edu.tr>
2024-10-28 20:23:36 -0400

    admin dashboard added

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'userprofile',
-    'courseEnroll'
+    'courseEnroll',
+    'systemadmin'
 ]
 
 MIDDLEWARE = [
