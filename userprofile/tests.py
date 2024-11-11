416ec2c08dc34cc9c00de0b8f4315a1005b22fbd
Shreyanshbhardwaj0709 <sb10261@nyu.edu>
2024-11-11 01:29:22 -0500

    models: modified TA student relationship; Modified test.py for userprofiles

--- userprofile/tests.py
+++ userprofile/tests.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from django.db import models
 from django.contrib.auth.models import User
 
-class StudentInfo(models.Model):
+class StudentInfoCourseEnrolledTestModel(models.Model):
     Edu_levels = [("Undergraduate", "Undergraduate"),
                   ("Graduate", "Graduate"),
                   ("PHD", "PHD")]
@@ -21,12 +21,12 @@ class StudentInfo(models.Model):
     Education_Level = models.CharField(max_length=50, choices=Edu_levels)
     Phone_no = models.CharField(max_length=15)  
     School = models.CharField(max_length=50, choices=Schools)
-    ta_course = models.ForeignKey('courseEnroll.CourseInfo', null=True, blank=True, related_name='tas', on_delete=models.SET_NULL)  # Use app label 'coursenroll'
+    ta_course = models.ForeignKey('courseEnroll.CourseInfo', null=True, blank=True, related_name='test_tas', on_delete=models.SET_NULL)  # Use app label 'coursenroll'
     is_ta = models.BooleanField(default=False)
-    course_enrolled = models.ManyToManyField('courseEnroll.CourseInfo', related_name='enrolled_students')  # Use app label 'coursenroll'
-    advisor = models.ForeignKey('AdminInfo', related_name='advising_students', on_delete=models.SET_NULL, null=True)
+    course_enrolled = models.ManyToManyField('courseEnroll.CourseInfo', related_name='test_enrolled_students')  # Use app label 'coursenroll'
+    advisor = models.ForeignKey('AdminInfo', related_name='test_advising_students', on_delete=models.SET_NULL, null=True)
 
-class AdminInfo(models.Model):
+class AdminInfoTestModel(models.Model):
     admin_id = models.CharField(max_length=9)
     Name = models.CharField(max_length=100, blank=True, null=True)
     email = models.EmailField()
@@ -35,7 +35,7 @@ class AdminInfo(models.Model):
     def __str__(self):
         return self.Name
 
-class DepartmentInfo(models.Model):
+class DepartmentInfoTestModel(models.Model):
     
     department_id = models.CharField(max_length=8, primary_key=True)
     name = models.CharField(default='CSE')
@@ -44,21 +44,21 @@ class DepartmentInfo(models.Model):
         return self.name 
 
 
-class FacultyInfo(models.Model):
+class FacultyInfoTestModel(models.Model):
     faculty_id = models.CharField(max_length=8, primary_key=True)
     Name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     Phone_no = models.CharField(max_length=15)
-    ta_students = models.ManyToManyField(StudentInfo, through='TA', related_name='faculty_tas')
+    ta_students = models.ManyToManyField(StudentInfoCourseEnrolledTestModel, through='TA', related_name='faculty_tas')
 
 
     def __str__(self):
         return self.Name 
 
-class TA(models.Model):
-    student = models.OneToOneField(StudentInfo, on_delete=models.CASCADE)
+class TATestModel(models.Model):
+    student = models.ForeignKey(StudentInfoCourseEnrolledTestModel, on_delete=models.CASCADE)
     course = models.ForeignKey('courseEnroll.CourseInfo', on_delete=models.CASCADE)  
-    faculty = models.ForeignKey(FacultyInfo, on_delete=models.CASCADE)
+    faculty = models.ForeignKey(FacultyInfoTestModel, on_delete=models.CASCADE)
 
     class Meta:
         unique_together = ('student', 'course')
